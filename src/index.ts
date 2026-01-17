a3f3f14ceef86f755c09c434449e97c289c941c6
HassanKhan123 <hk260280@gmail.com>
2026-01-17 15:29:09 +0100

    added: better-auth

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import cors from "cors";
+import { toNodeHandler } from "better-auth/node";
 import subjectsRouter from "./routes/subjects.js";
 import securityMiddleware from "./middleware/security.js";
+import { auth } from "./lib/auth.js";
 
 const app = express();
 const PORT = 8000;
@@ -17,6 +19,8 @@ app.use(
     credentials: true,
   })
 );
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
 
 app.use(securityMiddleware);
 

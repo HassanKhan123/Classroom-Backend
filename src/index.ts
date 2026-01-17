3851c18f76dfdff0451152379fc643335ef5116a
HassanKhan123 <hk260280@gmail.com>
2026-01-17 14:32:34 +0100

    added: arcjet for security

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import subjectsRouter from "./routes/subjects.js";
+import securityMiddleware from "./middleware/security.js";
 
 const app = express();
 const PORT = 8000;
@@ -16,6 +17,8 @@ app.use(
     credentials: true,
   })
 );
+
+app.use(securityMiddleware);
 
 app.use(express.json());
 
